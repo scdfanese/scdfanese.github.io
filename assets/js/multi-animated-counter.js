5ca7d538fc53c1323d2445d9d4adc8b3a93e1d20
Alessandro Lepore <lepore.ale@gmail.com>
2023-01-18 23:45:41 +0100

    Formatta JS

--- assets/js/multi-animated-counter.js
+++ assets/js/multi-animated-counter.js
@@ -1,8 +1,7 @@
 $(function () {
+  // ! Counter (used for Social Proof)
 
-    // ! Counter (used for Social Proof)
-
-    /* Usage example
+  /* Usage example
         <div id="counters_1">
             <div ="counter" data-TargetNum="10" data-Speed="6000"></div>
             <div class="counter" data-TargetNum="7" data-Speed="7000"
@@ -29,70 +28,78 @@ $(function () {
        Avoid to use this script in pages where it isn't needed
     */
 
-    // CONFIG
-    let visibilityIds = ['.counters']; //must be an array, could have only one element
-    let counterClass = '.counter';
-    let defaultSpeed = 3000; //default value
+  // CONFIG
+  let visibilityIds = [".counters"]; //must be an array, could have only one element
+  let counterClass = ".counter";
+  let defaultSpeed = 3000; //default value
 
-    // END CONFIG
+  // END CONFIG
 
-    //init if it becomes visible by scrolling
-    $(window).on('scroll', function () {
-        getVisibilityStatus();
-    });
-
-    //init if it's visible by page loading
+  //init if it becomes visible by scrolling
+  $(window).on("scroll", function () {
     getVisibilityStatus();
+  });
 
-    function getVisibilityStatus() {
-        elValFromTop = [];
-        var windowHeight = $(window).height(),
-            windowScrollValFromTop = $(this).scrollTop();
+  //init if it's visible by page loading
+  getVisibilityStatus();
 
-        visibilityIds.forEach(function (item, index) { //Call each class
-            try { //avoid error if class not exist
-                elValFromTop[index] = Math.ceil($(item).offset().top);
-            } catch (err) {
-                return;
-            }
-            // if the sum of the window height and scroll distance from the top is greater than the target element's distance from the top,
-            //it should be in view and the event should fire, otherwise reverse any previously applied methods
-            if ((windowHeight + windowScrollValFromTop) > elValFromTop[index]) {
-                counter_init(item);
-            }
-        });
-    }
+  function getVisibilityStatus() {
+    elValFromTop = [];
+    var windowHeight = $(window).height(),
+      windowScrollValFromTop = $(this).scrollTop();
 
-    function counter_init(groupId) {
-        let num, speed, direction, index = 0;
-        $(counterClass).each(function () {
-            num = $(this).attr('data-TargetNum');
-            speed = $(this).attr('data-Speed');
-            direction = $(this).attr('data-Direction');
-            easing = $(this).attr('data-Easing');
-            if (speed == undefined) speed = defaultSpeed;
-            $(this).addClass('c_' + index); //add a class to recognize each counter
-            doCount(num, index, speed, groupId, direction, easing);
-            index++;
-        });
-    }
+    visibilityIds.forEach(function (item, index) {
+      //Call each class
+      try {
+        //avoid error if class not exist
+        elValFromTop[index] = Math.ceil($(item).offset().top);
+      } catch (err) {
+        return;
+      }
+      // if the sum of the window height and scroll distance from the top is greater than the target element's distance from the top,
+      //it should be in view and the event should fire, otherwise reverse any previously applied methods
+      if (windowHeight + windowScrollValFromTop > elValFromTop[index]) {
+        counter_init(item);
+      }
+    });
+  }
 
-    function doCount(num, index, speed, groupClass, direction, easing) {
-        let className = groupClass + ' ' + counterClass + '.' + 'c_' + index;
-        if(easing == undefined) easing = "swing";
-        $(className).animate({
-            num
-        }, {
-            duration: +speed,
-            easing: easing,
-            step: function (now) {
-                if (direction == 'reverse') {
-                    $(this).text(num - Math.floor(now));
-                } else {
-                    $(this).text(Math.floor(now));
-                }
-            },
-            complete: doCount
-        });
-    }
-})
+  function counter_init(groupId) {
+    let num,
+      speed,
+      direction,
+      index = 0;
+    $(counterClass).each(function () {
+      num = $(this).attr("data-TargetNum");
+      speed = $(this).attr("data-Speed");
+      direction = $(this).attr("data-Direction");
+      easing = $(this).attr("data-Easing");
+      if (speed == undefined) speed = defaultSpeed;
+      $(this).addClass("c_" + index); //add a class to recognize each counter
+      doCount(num, index, speed, groupId, direction, easing);
+      index++;
+    });
+  }
+
+  function doCount(num, index, speed, groupClass, direction, easing) {
+    let className = groupClass + " " + counterClass + "." + "c_" + index;
+    if (easing == undefined) easing = "swing";
+    $(className).animate(
+      {
+        num,
+      },
+      {
+        duration: +speed,
+        easing: easing,
+        step: function (now) {
+          if (direction == "reverse") {
+            $(this).text(num - Math.floor(now));
+          } else {
+            $(this).text(Math.floor(now));
+          }
+        },
+        complete: doCount,
+      }
+    );
+  }
+});
